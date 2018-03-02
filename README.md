06494f6d4c3480a508d56099c06c1de8ef66457d
Jennifer Van <minh.j.van@gmail.com>
2018-03-02 14:05:21 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # selfie-print
 3D Print the outline contours from a selfie
+
+# Requirements
+
+Install OpenCV
+
+Install DLIB
+
+Install face_recognition
+
+# Step 1: Take a Selfie
+![](/static/selfie.jpg "Input Selfie, Format JPG")
+
+# Step 2: Recognize face, create bounding box, and crop
+![](/static/croppedselfie.jpg "Cropped Selfie")
+
+# Step 3: Find edges, find and draw contours
---
+++ identify_crop.py
@@ -0,0 +1,23 @@
+import face_recognition
+from PIL import Image, ImageDraw
+
+# This is an example of running face recognition on a single image
+# and drawing a box around each person that was identified.
+
+# Load an image
+img = face_recognition.load_image_file("selfie_face1.png")
+
+# Find all the faces and face encodings in the unknown image
+face_locations = face_recognition.face_locations(img)
+face_encodings = face_recognition.face_encodings(img, face_locations)
+
+# Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+pil_image = Image.fromarray(img)
+
+# Loop through each face found in the unknown image
+for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    PIL_box=[left-50, top-150, right+50, bottom+50]
+    crop_img = pil_image.crop(PIL_box)
+
+# You can also save a copy of the new image to disk if you want by uncommenting this line
+crop_img.save("croppedselfie.jpg")
